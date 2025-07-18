abc1cedaba3e9505f953f1e51e925c2164acd29c
YandaDev <nhlebelasn@protonmail.com>
2025-07-18 20:48:10 +0200

    Fix MongoDB connection options for Mongoose 8.x compatibility

--- server/server.js
+++ server/server.js
@@ -15,9 +15,7 @@ const connectDB = async () => {
       // Connection pooling options
       maxPoolSize: 10, // Maximum number of connections in the pool
       serverSelectionTimeoutMS: 5000, // Keep trying to send operations for 5 seconds
-      socketTimeoutMS: 45000, // Close sockets after 45 seconds of inactivity
-      bufferMaxEntries: 0, // Disable mongoose buffering
-      bufferCommands: false, // Disable mongoose buffering
+      socketTimeoutMS: 45000, // Close sockets after 45 seconds
     });
     
     console.log(`MongoDB Connected: ${conn.connection.host}`);
